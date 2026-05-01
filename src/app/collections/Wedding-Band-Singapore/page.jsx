2851745728a5f61bf7e170f3faa4cfa9b0cd6cec
Mworld4567 <domain100@brainoorja.com>
2026-05-01 19:17:01 +0530

    Engagement Ring Singapore Add For SEO

--- src/app/collections/Wedding-Band-Singapore/page.jsx
+++ src/app/collections/Wedding-Band-Singapore/page.jsx
@@ -1,4 +1,4 @@
-﻿import KihlasormusMiehelleCollectionClient from "./weddingBandSingaporeCollectionClient";
+﻿import WeddingBandSingaporeCollectionClient  from "./weddingBandSingaporeCollectionClient";
 
 export const metadata = {
   title:
@@ -7,6 +7,6 @@ export const metadata = {
     "Explore wedding bands crafted with precision and care. Elegant Nordic designs made for lasting love by Korkeila Helsinki.",
 };
 
-export default function KihlasormusMiehelleCollectionPage() {
-  return <KihlasormusMiehelleCollectionClient />;
+export default function WeddingBandSingaporePage() {
+  return <WeddingBandSingaporeCollectionClient />;
 }
