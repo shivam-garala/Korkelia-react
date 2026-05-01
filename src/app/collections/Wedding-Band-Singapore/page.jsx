a41384f80113195fa29739904b1214e514d5802f
Mworld4567 <domain100@brainoorja.com>
2026-05-01 18:16:50 +0530

    wedding band page add SEO

---
+++ src/app/collections/Wedding-Band-Singapore/page.jsx
@@ -0,0 +1,12 @@
+﻿import KihlasormusMiehelleCollectionClient from "./weddingBandSingaporeCollectionClient";
+
+export const metadata = {
+  title:
+    "Wedding Bands Singapore | Korkeila Helsinki",
+  description:
+    "Explore wedding bands crafted with precision and care. Elegant Nordic designs made for lasting love by Korkeila Helsinki.",
+};
+
+export default function KihlasormusMiehelleCollectionPage() {
+  return <KihlasormusMiehelleCollectionClient />;
+}
